0ab7056a7dc219cc07edde38b643a84240f76df8
curtiskueker <curtiskueker@gmail.com>
2017-09-08 10:25:25 -0700

    add session handling

--- controller/src/main/java/org/curtis/controller/AppServlet.java
+++ controller/src/main/java/org/curtis/controller/AppServlet.java
@@ -1,4 +1,16 @@
 package org.curtis.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/")
 public class AppServlet {
+    @RequestMapping(value = "/")
+    public ModelAndView main() {
+        ModelAndView modelAndView = new ModelAndView("main");
+
+        return modelAndView;
+    }
 }
